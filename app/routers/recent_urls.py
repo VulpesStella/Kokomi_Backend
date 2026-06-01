ca38bb64dbb1c9ff9510090cdc1e260bbd04c848
MaoYu <3197206779@qq.com>
2026-06-01 13:57:59 +0800

    fix: 调整recent功能接口

--- app/routers/recent_urls.py
+++ app/routers/recent_urls.py
@@ -7,7 +7,7 @@ from app.utils import GameUtils
 
 router = APIRouter(prefix='/recent')
 
-@router.get("/users/{user_id}/summary/", summary="")
+@router.get("/users/{user_id}/summary/", summary="获取用户近期数据概览")
 async def getRecentSummary(
     user_id: int = Path(..., description="用户ID"),
 ):
@@ -21,16 +21,30 @@ async def getRecentSummary(
 
     return result
 
-@router.get("/users/{user_id}/ranked/", summary="")
-async def getRankedRecent(
-    user_id: int = Path(..., description="用户ID"),
-):
-    if EnvConfig.DEV_MODE:
-        return JSONResponse.API_2018_Maintenance
+# @router.get("/users/{user_id}/random/", summary="获取用户近期随机数据")
+# async def getRandomRecent(
+#     user_id: int = Path(..., description="用户ID"),
+# ):
+#     if EnvConfig.DEV_MODE:
+#         return JSONResponse.API_2018_Maintenance
     
-    if GameUtils.check_uid(user_id) == False:
-        return JSONResponse.API_2001_IllegalAccountID
+#     if GameUtils.check_uid(user_id) == False:
+#         return JSONResponse.API_2001_IllegalAccountID
     
-    result = await RecentAPI.ranked(user_id, None, None)
+#     result = await RecentAPI.ranked(user_id, None, None)
 
-    return result
+#     return result
+
+# @router.get("/users/{user_id}/ranked/", summary="获取用户近期排位数据")
+# async def getRankedRecent(
+#     user_id: int = Path(..., description="用户ID"),
+# ):
+#     if EnvConfig.DEV_MODE:
+#         return JSONResponse.API_2018_Maintenance
+    
+#     if GameUtils.check_uid(user_id) == False:
+#         return JSONResponse.API_2001_IllegalAccountID
+    
+#     result = await RecentAPI.ranked(user_id, None, None)
+
+#     return result
